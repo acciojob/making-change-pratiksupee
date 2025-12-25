34ffc4e32d7b02f7727ca66ecb9a5d46e2732315
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2025-12-25 13:02:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const makeChange = (c) => {
-  // your name here
+  // your name 
+	c = Number(c);
+
+  let q = Math.floor(c / 25);
+  c = c % 25;
+
+  let d = Math.floor(c / 10);
+  c = c % 10;
+
+  let n = Math.floor(c / 5);
+  c = c % 5;
+	
+
+  let p = c;
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
